f5004f945195567545fd729912a7b1d0e0b99b70
Anisha Kalgi <k_anisha@icloud.com>
2026-04-17 19:40:25 -0700

    lab3 additions

---
+++ global.js
@@ -0,0 +1,168 @@
+// // console.log('IT’S ALIVE!');
+
+// // function $$(selector, context = document) {
+// //   return Array.from(context.querySelectorAll(selector));
+// // }
+
+// // let pages = [
+// //   { url: 'index.html', title: 'Home' },
+// //   { url: 'projects/', title: 'Projects' },
+// //   {url: 'resume/', title: 'Resume' },
+// //   {url: 'contact/', title: 'Contact '}
+// // ];
+
+// // const BASE_PATH =
+// //   (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+// //     ? "/"
+// //     : "/portfolio/"; 
+
+// // let nav = document.createElement('nav');
+// // document.body.prepend(nav);
+
+// // for (let p of pages) {
+// //   let url = p.url;
+// //   let title = p.title;
+
+// //   url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+// //   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+// // }
+
+// // for (let p of pages) {
+// //   let url = p.url;
+// //   let title = p.title;
+
+// //   let a = document.createElement('a');
+// //   a.href = url;
+// //   a.textContent = title;
+
+// //   nav.append(a);
+// // }
+
+// // const navLinks = $$("nav a");
+
+// // let currentLink = navLinks.find(
+// //   (a) => a.host === location.host && a.pathname === location.pathname
+// // );
+
+// // currentLink?.classList.add('current');
+
+
+// console.log('IT’S ALIVE!');
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// let pages = [
+//   { url: 'index.html', title: 'Home' },
+//   { url: 'projects/index.html', title: 'Projects' },
+//   { url: 'resume/index.html', title: 'Resume' },
+//   { url: 'contact/index.html', title: 'Contact' }
+// ];
+
+// let nav = document.createElement('nav');
+// document.body.prepend(nav);
+
+// for (let p of pages) {
+//   let a = document.createElement('a');
+//   a.href = BASE_PATH + p.url;
+//   a.textContent = p.title;
+//   nav.append(a);
+// }
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find((a) => {
+//   return (
+//     a.pathname === location.pathname ||
+//     (location.pathname === '/' && a.pathname.endsWith('index.html'))
+//   );
+// });
+
+// currentLink?.classList.add('current');
+
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: 'index.html', title: 'Home' },
+  { url: 'projects/index.html', title: 'Projects' },
+  { url: 'resume/index.html', title: 'Resume' },
+  { url: 'contact/index.html', title: 'Contact' }
+];
+
+const BASE_PATH =
+  (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"
+    : "/portfolio/";
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="auto">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+let select = document.querySelector(".color-scheme select");
+
+if ("colorScheme" in localStorage) {
+  let scheme = localStorage.colorScheme;
+
+  document.documentElement.style.setProperty("color-scheme", scheme);
+
+  select.value = scheme;
+}
+
+select.addEventListener("input", (event) => {
+  console.log("color scheme changed to", event.target.value);
+
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    event.target.value
+  );
+
+  localStorage.colorScheme = event.target.value;
+});
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  let fullURL = url.startsWith('http') ? url : BASE_PATH + url;
+
+  let a = document.createElement('a');
+  a.href = fullURL;
+  a.textContent = title;
+
+if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find((a) =>
+  a.pathname === location.pathname
+);
+
+currentLink?.classList.add('current');
