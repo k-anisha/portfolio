374e8700a117677a70b93bec010c8c8a03b093f7
Anisha Kalgi <k_anisha@icloud.com>
2026-04-25 17:39:44 -0700

    lab4 updates

--- projects/projects.js
+++ projects/projects.js
@@ -5,3 +5,7 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const numProjects = projects.length
+const nProj = document.querySelector(".projects-title")
+nProj.innerHTML = `<h1>${numProjects} Projects</h1>`
