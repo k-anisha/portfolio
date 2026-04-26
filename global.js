3ace4987d62fcb880e1fc0bd2c2e773f6c9cc2c8
Anisha Kalgi <k_anisha@icloud.com>
2026-04-25 17:25:12 -0700

    lab 4 additions

--- global.js
+++ global.js
@@ -148,7 +148,7 @@ for (let p of pages) {
   a.href = fullURL;
   a.textContent = title;
 
-if (a.host === location.host && a.pathname === location.pathname) {
+  if (a.host === location.host && a.pathname === location.pathname) {
     a.classList.add('current');
   }
 
@@ -166,3 +166,35 @@ let currentLink = navLinks.find((a) =>
 );
 
 currentLink?.classList.add('current');
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  project.map((p) => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <${headingLevel}>${p.title}</${headingLevel}>
+    <img src="${p.image}" alt="${p.title}">
+    <p>${p.description}</p>
+  `;
+    containerElement.appendChild(article);
+  })
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
